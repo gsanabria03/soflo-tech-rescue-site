0d82c2593ea2c80e90ed5332c3729471238f2c7f
gsanabria03 <142358070+gsanabria03@users.noreply.github.com>
2026-03-08 12:42:46 -0400

    Update animations.js

--- animations.js
+++ animations.js
@@ -36,8 +36,8 @@ scrollLinks.forEach(link => {
 });
 
 const burger = document.querySelector(".burger");
-const navLinks = document.querySelector(".nav-links");
+const nav = document.querySelector("nav");
 
 burger.addEventListener("click", () => {
-  navLinks.classList.toggle("active");
+  nav.classList.toggle("active");
 });
